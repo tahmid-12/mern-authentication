060209fc0706ff1a82b6d38ed661530881d1217a
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-09-12 20:21:57 +0600

    Log Out API Created

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { loginHandler, registerHandler } from "../controllers/authController";
+import { loginHandler, logoutHandler, refreshTokenHandler, registerHandler } from "../controllers/authController";
 
 const authRoutes = Router();
 
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
+authRoutes.get("/refresh-token", refreshTokenHandler);
+authRoutes.post("/logout", logoutHandler);
 
 export default authRoutes;
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -15,3 +15,7 @@ export const fifteenMinutesFromNow = () => {
         Date.now() + 15 * 60 * 1000
     )
 }
+
+export const ONE_DAY_MS = () => {
+    return 24 * 60 * 60 * 1000;
+}
